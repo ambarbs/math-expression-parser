1a0422e82a65646ccc0d8fa840e9fef834d54f6b
Ambar Bose <ambarbs@users.noreply.github.com>
2026-06-24 11:11:11 +1000

    feat: add AST model for arithmetic and comparison expressions

---
+++ src/parser/ast.ts
@@ -0,0 +1,28 @@
+export type ArithmeticOperator = '+' | '-' | '*' | '/';
+export type ComparisonOperator = '=' | '!=';
+export type UnaryOperator = '-';
+
+export type NumberLiteralNode = {
+  type: 'NumberLiteral';
+  value: number;
+};
+
+export type BinaryExpressionNode = {
+  type: 'BinaryExpression';
+  operator: ArithmeticOperator;
+  left: ArithmeticExpressionNode;
+  right: ArithmeticExpressionNode;
+};
+
+export type ArithmeticExpressionNode = NumberLiteralNode | BinaryExpressionNode;
+
+export type ComparisonExpressionNode = {
+  type: 'ComparisonExpression';
+  operator: ComparisonOperator;
+  left: ArithmeticExpressionNode;
+  right: ArithmeticExpressionNode;
+};
+
+export type ExpressionNode =
+  | ArithmeticExpressionNode
+  | ComparisonExpressionNode;
